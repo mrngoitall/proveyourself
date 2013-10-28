6b26378697484fdab864414ef047b4729e4efd3b
William Ngo <will@mrngoitall.net>
2013-10-28 12:22:54 -0700

    Fixed issue with question 1 in directive

--- app/scripts/directives/QuizDirective.js
+++ app/scripts/directives/QuizDirective.js
@@ -4,10 +4,6 @@ angular.module('DeskAngularJSApp')
   .directive('answer', function () {
     return {
       restrict: 'E',
-      scope: {
-        options: '=',
-        answermodel: '@'
-      },
       template: '<div class="radio">\
         <label>\
           <input type="radio" ng-model="userAnswers.a1" name="a1" value="{{ options.value }}">{{ options.answer }}\
