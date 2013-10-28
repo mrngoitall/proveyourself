e932ab26bff14dcaab1070464c20a93859e221a7
William Ngo <will@mrngoitall.net>
2013-10-27 23:57:23 -0700

    Moved user answers to userAnswers object in scope

--- app/scripts/directives/QuizDirective.js
+++ app/scripts/directives/QuizDirective.js
@@ -5,11 +5,12 @@ angular.module('DeskAngularJSApp')
     return {
       restrict: 'E',
       scope: {
-        options: '='
+        options: '=',
+        answermodel: '@'
       },
       template: '<div class="radio">\
         <label>\
-          <input type="radio" ng-model="a1" name="a1" value="{{ options.value }}">{{ options.answer }}\
+          <input type="radio" ng-model="userAnswers.a1" name="a1" value="{{ options.value }}">{{ options.answer }}\
         </label>\
       </div>',
       link: function postLink(scope, element, attrs) {
