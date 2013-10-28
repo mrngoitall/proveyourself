9fb0563990fe2fd90cf853a740b93b10a6e6f05d
William Ngo <will@mrngoitall.net>
2013-10-28 13:59:32 -0700

    Updated main controller test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have an object of answers in the scope', function () {
+    expect(Object.keys(scope.answers).length).toBe(7);
   });
 });
