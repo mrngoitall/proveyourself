a1e99e916d592ac98bc26a1f7062a29ff4c70827
William Ngo <will@mrngoitall.net>
2013-10-29 11:27:20 -0700

    Added styling for right/wrong answers in question 1 directive

--- app/scripts/directives/QuizDirective.js
+++ app/scripts/directives/QuizDirective.js
@@ -4,9 +4,13 @@ angular.module('DeskAngularJSApp')
   .directive('answer', function () {
     return {
       restrict: 'E',
-      template: '<div class="radio">\
+      template: '<div class="radio" ng-class="{\
+        \'bs-callout\': score > -1 && (options.value == answers.a1 || options.value == userAnswers.a1),\
+        \'bs-callout-success\': score > -1 && options.value == answers.a1,\
+        \'bs-callout-danger\': score > -1 && options.value == userAnswers.a1 && options.value != answers.a1\
+      }">\
         <label>\
-          <input type="radio" ng-model="userAnswers.a1" name="a1" value="{{ options.value }}">{{ options.answer }}\
+          <input type="radio" class="control-label" ng-model="userAnswers.a1" name="a1" value="{{ options.value }}" ng-disabled="score > -1">{{ options.answer }}\
         </label>\
       </div>',
       link: function postLink(scope, element, attrs) {
