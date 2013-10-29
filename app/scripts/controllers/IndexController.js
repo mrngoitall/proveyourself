6cc5df2ff8e10fb60b230eafec44023a715699ad
William Ngo <will@mrngoitall.net>
2013-10-28 21:02:19 -0700

    Adding redirect for users who are already logged in

--- app/scripts/controllers/IndexController.js
+++ app/scripts/controllers/IndexController.js
@@ -5,6 +5,12 @@ angular.module('DeskAngularJSApp')
 
   $scope.email = localStorage.email || '';
 
+  if ($location.path() === '/quiz' && !$scope.email.length) {
+    $location.path('/');
+  } else if ($location.path() === '/register' && $scope.email.length) {
+    $location.path('/quiz');
+  }
+
   $scope.register = function() {
     if ($scope.registration.$valid) {
       localStorage.email = $scope.email;
